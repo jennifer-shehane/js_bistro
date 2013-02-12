b62d6d460ee3951024e4c402398cb5a687d81a35
Jennifer Shehane <shehane.jennifer@gmail.com>
2013-02-12 17:02:33 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JsBistro::Application.config.secret_token = 'd8b00da836ab5e1a9fb17d9b899299ec3a737db225d3044cc618817991a66798da38b40a27319c13522814088fdc72b51f316d851830a3685b01e8d5f46de578'
